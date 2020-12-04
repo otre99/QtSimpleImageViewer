26db99423120b775c0f7b6daf4f5fab5d35c57de
Roberto Cruz <roberto.cruz@admobilize.com>
2020-12-04 14:36:26 -0600

    avoid starting in full screen

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow w;
-  w.show();
-  w.setGeometry(QApplication::screens()[0]->geometry());
+  w.showMaximized();
+  //w.setGeometry(QApplication::screens()[0]->geometry());
   const QStringList args = QApplication::arguments();
   if (args.size() > 1) {
     w.LoadImage(args[1]);
